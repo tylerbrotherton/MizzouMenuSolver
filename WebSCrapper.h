629d01fd4c00a27d40d72ff64eeefc7f79e1b53a
Tyler Brotherton <105938811+tylerbrotherton@users.noreply.github.com>
2025-10-27 22:53:35 -0500

    Create WebSCrapper.h

---
+++ WebSCrapper.h
@@ -0,0 +1,7 @@
+
+
+
+static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp);
+std::string fetchPage(const std::string& url);
+std::vector<std::string> parseMenu(std::string& text);
+int isValid(int argc, char* argv[]);
