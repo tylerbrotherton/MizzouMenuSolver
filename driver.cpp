5c507a1c5dcf9bb5f995ba23360242893caa86c6
Tyler Brotherton <105938811+tylerbrotherton@users.noreply.github.com>
2025-10-29 16:49:49 -0500

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -12,12 +12,16 @@ int main(){
     //String diningHallsAtMizzou[3]= {"plaza-900-dining","the-mark-on-5th-street","the-restaurants-at-southwest"};
       
     for(String i: diningHallsAtMizzou){
+        if(wb.isValid("https:////dining.missouri.edu//locations//"+i+'//'))
+            
         std::vector<std::string> items;
         items=wb.fectpage("https:////dining.missouri.edu//locations//"+i+'//');
         int success=g.displayGraphics(items);
         if(success==0) cout<<"Success for " <<i;
     }
 
+
+    
     /*
     *
     * Do graphics?
