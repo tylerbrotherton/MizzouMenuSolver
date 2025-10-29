855fedff53889d5c619618631a0dd2dd6485b3a8
Tyler Brotherton <105938811+tylerbrotherton@users.noreply.github.com>
2025-10-29 16:11:24 -0500

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -6,13 +6,12 @@
 #include "Graphics.h"
 
 int main(){
-    std::vector<std::string> diningHallsAtMizzou;
-    diningHallsAtMizzou.append("plaza-900-dining")
+    String diningHallsAtMizzou[3]= {"plaza-900-dining","the-mark-on-5th-street","the-restaurants-at-southwest"};
 
       
-  
-    std::vector<std::string> items;
-    items=fectpage("https:////dining.missouri.edu//locations//"+diningHallsAtMizzou.at(0)+'//');
-    int displayGraphicsReturn=displayGraphics(items);
-      
+    for(String i: diningHallsAtMizzou)
+        std::vector<std::string> items;
+        items=fectpage("https:////dining.missouri.edu//locations//"+diningHallsAtMizzou.at(0)+'//');
+        int success=displayGraphics(items);
+        if(success==0) cout<<"Success for " <<i;
 }
